f75c06d5cbba2e11a03a79e802f2cff723bcc1ca
jefferson.valente <jeffersonvalente1988@gmail.com>
2023-10-14 12:09:25 -0300

    added: spn terraform

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "rolespn" {
-  scope= var.SUBSCRIPTION_ID
+  scope= "/subscriptions/${var.SUBSCRIPTION_ID}"
   role_definition_name= "Contributor"
   principal_id = var.AKS_SPN
 }
