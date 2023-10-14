5e98b28c5d856690a15b68f4a9985fd0f6e3c781
jefferson.valente <jeffersonvalente1988@gmail.com>
2023-10-13 23:25:36 -0300

    added: module serviceprincipal/output.tf

--- infrastructure/modules/servicePrincipal/main.tf
+++ infrastructure/modules/servicePrincipal/main.tf
@@ -10,3 +10,7 @@ resource "azuread_service_principal" "example" {
     app_role_assignment_required = true
     owners = [data.azuread_client_config.current.object_id]
 }
+
+resource "azuread_service_principal_password" "spnpass" {
+    service_principal_id = azuread_service_principal.example.object_id
+}
